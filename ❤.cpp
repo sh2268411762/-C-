d489bbf3883d28c459472fa505c8b940e765b356
sh2268411762 <2268411762@qq.com>
2020-11-06 23:49:15 +0800

    C

--- ❤.cpp
+++ ❤.cpp
@@ -1,10 +1,9 @@
-/*
-#include <stdio.h>
-#include <stdlib.h>
-int main()
-{
-	printf("   ***    ***\n**     **     **\n  **       **\n     **  **\n       **\n\nhello!\ndo you want to be my girlfriend?¡®_¡¯\n");
-	system("pause");
-	return 0;
-}
-*/
+//
+//#include <stdio.h>
+//#include <stdlib.h>
+//int main()
+//{
+//	printf("   ***    ***\n**     **     **\n  **       **\n     **  **\n       **\n\nhello!\ndo you want to be my girlfriend?¡®_¡¯\n");
+//	system("pause");
+//	return 0;
+//}
